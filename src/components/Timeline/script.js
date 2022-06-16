7be41d1c902829ac56dcdcafb98ee1ca2519944b
matheus faria <matheusbahia10@gmail.com>
2022-06-16 10:30:46 -0300

    timeline fix

--- src/components/Timeline/script.js
+++ src/components/Timeline/script.js
@@ -4,40 +4,47 @@ export function getTime() {
   const minutos = Number(date.getMinutes());
   const dateHour = Number(date.getHours());
 
-  switch (true) {
-    case dateHour >= 22 && minutos >= 15:
-      return 110;
+  const totalMinutes = dateHour * 60 + minutos;
+  console.log(totalMinutes);
 
-    case dateHour >= 21 && minutos >= 15:
+  switch (true) {
+    case totalMinutes < 690:
+      return 2;
+
+    case totalMinutes >= 1335:
+      return 100;
+
+    case totalMinutes >= 1275:
       return 93;
 
-    case dateHour >= 20 && minutos >= 15:
+    case totalMinutes >= 1215:
       return 86;
 
-    case dateHour >= 20 && minutos >= 0:
+    case totalMinutes >= 1200:
       return 77;
 
-    case dateHour >= 19 && minutos >= 0:
+    case totalMinutes >= 1140:
       return 65;
 
-    case dateHour >= 18 && minutos >= 0:
+    case totalMinutes >= 1080:
       return 58;
 
-    case dateHour >= 14 && minutos >= 15:
+    case totalMinutes >= 855:
       return 47;
 
-    case dateHour >= 13:
-      if (minutos >= 45) return 39;
-      if (minutos >= 30) return 30;
+    case totalMinutes >= 825:
       return 39;
 
-    case dateHour >= 12 && minutos >= 0:
+    case totalMinutes >= 810:
+      return 30;
+
+    case totalMinutes >= 720:
       return 20;
 
-    case dateHour >= 11 && minutos >= 30:
+    case totalMinutes >= 690:
       return 11;
 
     default:
-      return 2;
+      return 0;
   }
 }
